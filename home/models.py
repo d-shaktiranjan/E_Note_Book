7243bd68164a70f341794be07e4b82dc016260de
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-01-03 21:21:34 +0530

    db created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class NoteBook(models.Model):
+    noteName = models.CharField(max_length=10)
+    about = models.CharField(max_length=25)
+    teachers = models.CharField(max_length=20)
+    dateTime = models.DateTimeField()
+
+    def __str__(self):
+        return self.noteName
