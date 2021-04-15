b54cb282c8c9471ffa3d92f5e1c70f5416326dba
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-04-15 13:41:36 +0530

    function added to generate otp

--- home/signupFunctions.py
+++ home/signupFunctions.py
@@ -1,5 +1,12 @@
 from home.models import UsersData
+from random import randint
 
 
 def checkMail(mail):
     return bool(UsersData.objects.filter(mail=mail))
+
+
+def otpGenerate():
+    range_start = 10**(6-1)
+    range_end = (10**6)-1
+    return randint(range_start, range_end)
