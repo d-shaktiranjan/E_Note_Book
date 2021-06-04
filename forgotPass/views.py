461108a88de0a682c6cd228cb30ddf2a069e5ed8
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-06-04 12:29:42 +0530

    if already loggedin redirect to index

--- forgotPass/views.py
+++ forgotPass/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from home.views import index
 
 # Create your views here.
 
 
 def index(request):
+    if (request.session.get('log')):
+        return redirect(index)
     return render(request, "fIndex.html")
