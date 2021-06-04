4e824e667ed3eeaf59fc33700e0e20f291732ba3
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-06-04 14:31:51 +0530

    reset password functionality done😁

--- forgotPass/urls.py
+++ forgotPass/urls.py
@@ -4,4 +4,5 @@ from forgotPass import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('otpcheck/', views.otpcheck, name="otpcheckForgot"),
+    path('resetPass/', views.resetPass, name="resetPass"),
 ]
--- forgotPass/views.py
+++ forgotPass/views.py
@@ -4,10 +4,11 @@ from home.models import UsersData
 from django.core.mail import send_mail
 from django.conf import settings
 from home.signupFunctions import otpGenerate
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
-userDict = {}
+userDict = {"otpMatch": False}
 
 
 def index(request):
@@ -42,6 +43,25 @@ def otpcheck(request):
     if request.method == "POST":
         userOtp = request.POST.get('otp')
         if userOtp == userDict["otp"]:
+            userDict["otpMatch"] = True
             return render(request, "resetPassword.html")
         return error(request, "OTP not matched", "Try Again", "/forgot")
     return render(request, "forgotOtpCheck.html")
+
+
+def resetPass(request):
+    if request.session.get('log'):
+        return redirect(homeIndex)
+    if userDict["otpMatch"]:
+        if request.method == "POST":
+            newPass = request.POST.get('newPass')
+            newConPass = request.POST.get('newConPass')
+            if newConPass == newPass:
+                userInfo = UsersData.objects.filter(
+                    mail=userDict['email']).first()
+                userInfo.password = make_password(newPass)
+                userInfo.save()
+                return error(request, "Password reset done", "Home", "/")
+            return error(request, "New password & confirm password not matched", "Try Again", "/forgot")
+        return redirect(index)
+    return redirect(index)
