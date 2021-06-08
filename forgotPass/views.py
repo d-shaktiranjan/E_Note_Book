9729cbfa0bd0ada5e95f1faa855920335f2e6ae3
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-06-08 10:06:16 +0530

    use DB to reset password

--- forgotPass/views.py
+++ forgotPass/views.py
@@ -5,50 +5,50 @@ from django.core.mail import send_mail
 from django.conf import settings
 from home.signupFunctions import otpGenerate
 from django.contrib.auth.hashers import make_password
+from forgotPass.models import ForgotTempData
 
 # Create your views here.
 
-GLOBAL_otp = ""
-GLOBAL_email = ""
-GLOBAL_match = False
-
 
 def index(request):
-    global GLOBAL_email, GLOBAL_otp
     if request.session.get('log'):
         return redirect(homeIndex)
     if request.method == "POST":
-        email = request.POST.get('email')
+        mail = request.POST.get('email')
         userInfo = UsersData.objects.filter(
-            mail=email).first()
+            mail=mail).first()
         if userInfo == None:
             return error(request, "Your mail is not registered to us", "Home", "/")
         otp = otpGenerate()
-        GLOBAL_email = email
-        GLOBAL_otp = str(otp)
         try:
             send_mail(
                 "OTP | E Note Book",
                 f'Hey your OTP to reset password is {otp}',
                 settings.EMAIL_HOST_USER,
-                [email],
+                [mail],
                 fail_silently=False,
             )
+            tempUser = ForgotTempData(otp=otp, mail=mail)
+            tempUser.save()
+            userDict = {"mail": mail}
         except:
             return error(request, "There are some internal issue", "Home", "/")
-        return render(request, "forgotOtpCheck.html")
+        return render(request, "forgotOtpCheck.html", userDict)
     return render(request, "fIndex.html")
 
 
 def otpcheck(request):
-    global GLOBAL_match
     if request.session.get('log'):
         return redirect(homeIndex)
     if request.method == "POST":
+        mail = request.POST.get('mail')
         userOtp = request.POST.get('otp')
-        if userOtp == GLOBAL_otp:
-            GLOBAL_match = True
-            return render(request, "resetPassword.html")
+        tempUserData = ForgotTempData.objects.filter(mail=mail).first()
+        if userOtp == tempUserData.otp:
+            userDict = {"mail": tempUserData.mail}
+            tempUserData.delete()
+            return render(request, "resetPassword.html", userDict)
+        tempUserData.delete()
         return error(request, "OTP not matched", "Try Again", "/forgot")
     return render(request, "forgotOtpCheck.html")
 
@@ -56,16 +56,15 @@ def otpcheck(request):
 def resetPass(request):
     if request.session.get('log'):
         return redirect(homeIndex)
-    if GLOBAL_match:
-        if request.method == "POST":
-            newPass = request.POST.get('newPass')
-            newConPass = request.POST.get('newConPass')
-            if newConPass == newPass:
-                userInfo = UsersData.objects.filter(
-                    mail=GLOBAL_email).first()
-                userInfo.password = make_password(newPass)
-                userInfo.save()
-                return error(request, "Password reset done", "Home", "/")
-            return error(request, "New password & confirm password not matched", "Try Again", "/forgot")
-        return redirect(index)
+    if request.method == "POST":
+        newPass = request.POST.get('newPass')
+        newConPass = request.POST.get('newConPass')
+        mail = request.POST.get('mail')
+        if newConPass == newPass:
+            userInfo = UsersData.objects.filter(
+                mail=mail).first()
+            userInfo.password = make_password(newPass)
+            userInfo.save()
+            return error(request, "Password reset done", "Home", "/")
+        return error(request, "New password & confirm password not matched", "Try Again", "/forgot")
     return redirect(index)
