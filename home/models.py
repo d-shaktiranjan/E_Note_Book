260c2a2fd3231227bbb681931c091ccbd6abbc9a
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-06-24 11:44:48 +0530

    add new row in notebook db

--- home/models.py
+++ home/models.py
@@ -12,6 +12,7 @@ class NoteBook(models.Model):
     slug = models.CharField(max_length=25)
     content = models.TextField()
     bookOwner = models.CharField(max_length=20)
+    isPublic = models.BooleanField(default=False)
 
     def __str__(self):
         return self.noteName
