607fb1b9cbfd249c8f6014994ceb001dfac78c12
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2021-06-24 11:40:29 +0530

    share template added

--- share/views.py
+++ share/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from home.views import error
+from home.models import NoteBook
 
 # Create your views here.
 
 
 def share(request, slug):
-    return HttpResponse("Share Page")
+    if not request.session.get('log'):
+        return error(request, "Login first", "Home", "/")
+    notebook = NoteBook.objects.filter(slug=slug).first()
+    if notebook.bookOwner != request.session.get('userName'):
+        return error(request, "You are not allowed to edit this book", "Back to Home", "/")
+    return render(request, "share.html")
 
 
 def makePublic(request):
