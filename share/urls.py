84ff1ce9a95cbeb98dfd994384bc3764cba73153
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2022-01-27 22:27:33 +0530

    share UI added

--- share/urls.py
+++ share/urls.py
@@ -4,5 +4,6 @@ from share import views
 urlpatterns = [
     path('makePublic', views.makePublic, name="makePublic"),
     path('makePrivate', views.makePrivate, name="makePrivate"),
+    path('sharedList', views.sharedList, name="sharedList"),
     path('<slug:slug>/', views.share, name="share"),
 ]
